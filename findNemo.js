2ad7cb6a3cccfdc6d68a78e1098da7ece46f2f2f
aboozarmojdeh <aboozar.mojdeh@gmail.com>
2020-10-08 12:18:07 -0700

    Big O examples ended

--- findNemo.js
+++ findNemo.js
@@ -1,7 +1,8 @@
 const {performance} = require("perf_hooks")
-let large=new Array(10000).fill('nemo');
+let large=new Array(1000).fill('nemo');
+const everyone=['dory','bruce','nemo','marlin','nigel','squirt']
 
-function findNemo(array){ //O(n) ---> Linear Time
+function findNemo1(array){ //O(n) ---> Linear Time
   const t0=performance.now();
   for (let i=0;i<array.length;i++){
     if(array[i]==='nemo'){
@@ -13,4 +14,25 @@ function findNemo(array){ //O(n) ---> Linear Time
 //O(n) ---> Linear Time
 };
 
-findNemo(large);
+findNemo1(everyone);
+
+const findNemo2=(array)=>{//O(n) ---> Linear Time
+array.forEach(fish=>{
+  if(fish==='nemo'){
+    console.log('Found Nemo!')
+  }
+})
+};
+
+findNemo2(everyone)
+
+const findNemo3=(array)=>{//O(n) ---> Linear Time
+for(fish of array){
+  if(fish==='nemo'){
+    console.log('Found Nemo!')
+  }
+}
+}
+
+
+findNemo3(everyone)
---
+++ lengthMethod.js
@@ -0,0 +1,6 @@
+",sdjflksjdflsdf".length; //O(1)
+[1,2,3,4,5].length;  //O(1)
+
+
+console.log(",sdjflksjdflsdf".length);
+console.log([1,2,3,4,5].length);
